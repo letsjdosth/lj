269d49d0114efee69a78e652c2a9d2c76658cc54
Seokjun Choi <rtoru@outlook.com>
2019-01-08 05:10:48 +0900

    lint installed

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "off" //eslint:recommended에서는 켜져있음. 실무 목적에서는 콘솔 출력을 하지 않는게 좋음.
+        //기타: 스트링 따옴표 설정은 삭제함.
+    }
+};
